b972b0bb047f4884b16c4f8268f1db491502ec80
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-08-05 11:55:02 +0100

    Change file path to images

--- Package/V0.0.0-beta/Scripts/pages.py
+++ Package/V0.0.0-beta/Scripts/pages.py
@@ -77,12 +77,12 @@ def title_func(title_name):
     title, logo = st.beta_columns([5, 1])
 
     title.title(title_name)
-    logo.image('../Images/Logo.png', width=100)
+    logo.image('https://github.com/Password-Classified/Stock-Search/blob/master/Package/V0.0.0-beta/Images/logo.png', width=100)
 
 
 # Pages
 def welcome():
-    st.image('../Images/logo.png', width=520)
+    st.image('https://github.com/Password-Classified/Stock-Search/blob/master/Package/V0.0.0-beta/Images/logo.png', width=520)
     st.write(f'''
 # Stock Search
 
